10cb0d947af49f8bfe8c4b8ff69c3cce46780532
Bhuvanakalaga <bhuvanakalaga@gmail.com>
2026-06-03 00:29:00 +0530

    Initial project setup

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+      <img src={assets.logo} alt="" width={150}/>
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright © 2026 Bhuvana | All rights reserved</p>
+      <div className='flex gap-2.5'>
+           <a
+          href="https://www.linkedin.com/in/bhuvana-kalaga-b8805828b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={assets.linkedin_icon} alt="" width={35} />
+        </a>
+
+        {/* GitHub */}
+        <a
+          href="https://github.com/sravyapalla/Dreamsketch"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={assets.github_icon} alt="" width={35} />
+        </a>
+      
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Generate from '../components/Generate'
+import StylePicker, { STYLE_PRESETS } from "../components/stylePicker";
+import { motion } from "framer-motion";
+
+
+const Home = () => {
+    const [style, setStyle] = useState("none");
+  const [provider, setProvider] = useState("stability");
+  return (
+    
+   <div>
+    <Header/>
+    <Steps/>
+    
+    <Description/>
+    
+<div className="px-4 my-3">
+  <motion.p
+    initial={{ opacity: 0.2, y: 100 }}
+    transition={{ duration: 1 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    viewport={{ once: true }}
+    className="text-center text-2xl md:text-4xl font-extrabold mb-5
+               text-transparent bg-clip-text
+               bg-linear-to-r from-black-200 via-teal-500 to-orange-600
+               drop-shadow-sm"
+  >
+    Pick your vibe, choose your wizard, and let’s conjure some pixels!
+  </motion.p>
+
+  <StylePicker
+    style={style}
+    setStyle={setStyle}
+    provider={provider}
+    setProvider={setProvider}
+  />
+</div>
+
+  <Generate
+  provider={provider}
+  styleSuffix={STYLE_PRESETS?.[style] ?? ""}
+/>
+
+   </div>
+  )
+}
+
+export default Home
